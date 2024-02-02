65774ba11705a19557e0cae9f4837cece2b19aca
gozuacik <gozuacik@gmail.com>
2024-02-02 08:48:35 +0300

    Update OpenAI usage

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import streamlit as st
 st.title("ChatGPT-like clone")
 
 #client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-client = OpenAI(api_key=OPENAI_API_KEY)
+#client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI()
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
